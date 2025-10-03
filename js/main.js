d6ea60c29e150586f48441343259e19ee6c8d589
PrajwalGaniga <prajwalganiga06@gmail.com>
2025-10-03 16:07:30 +0530

    Your commit message describing the update

--- js/main.js
+++
@@ -1,7 +0,0 @@
-// Simple navbar scroll effect
-window.addEventListener("scroll", () => {
-  const header = document.querySelector("header");
-  header.classList.toggle("sticky", window.scrollY > 50);
-});
-
-// You can add more animations with AOS.js or GSAP here later
